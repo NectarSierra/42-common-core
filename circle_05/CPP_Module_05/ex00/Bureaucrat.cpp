214441d41882c035caab29dae21f3a7df3dd42f3
Nathan Saillez <nsaillez@shi-r6-p1.s19.be>
2025-12-08 12:12:05 +0100

    Implemented the orthodox canonical form for Bureaucrat class

--- circle_05/CPP_Module_05/ex00/Bureaucrat.cpp
+++ circle_05/CPP_Module_05/ex00/Bureaucrat.cpp
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsaillez <nsaillez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 11:57:06 by nsaillez          #+#    #+#             */
+/*   Updated: 2025/12/08 12:07:49 by nsaillez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat( std::string r_name)
+Bureaucrat::Bureaucrat(void) : name(""), grade(150)
 {
-
+	std::cout << "Bureaucrat default constructor called!" << std::endl;
 }
 
-Bureaucrat::Bureaucrat(Bureaucrat &obj)
+Bureaucrat::Bureaucrat(std::string r_name, int r_grade) : name(r_name), grade(r_grade)
 {
+	std::cout << "Bureaucrat parameterized constructor called!" << std::endl;
+}
 
+Bureaucrat::Bureaucrat(const Bureaucrat &obj) : name(obj.name), grade(obj.grade) 
+{
+	std::cout << "Bureaucrat copy constructor called!" << std::endl;
 }
 
 Bureaucrat::~Bureaucrat()
 {
-
-}
-
-Bureaucrat Bureaucrat::&operator=(const Bureaucrat &obj)
-{
-
+	std::cout << "Bureaucrat destructor called!" << std::endl;
 }
--- circle_05/CPP_Module_05/ex00/Bureaucrat.hpp
+++ circle_05/CPP_Module_05/ex00/Bureaucrat.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsaillez <nsaillez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 11:57:16 by nsaillez          #+#    #+#             */
+/*   Updated: 2025/12/08 12:10:18 by nsaillez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef BUREAU_CRAT_HPP
 #define BUREAU_CRAT_HPP
 
@@ -8,11 +20,20 @@ class Bureaucrat
 	private:
 		const std::string name;
 		int grade;
+	
 	public:
-		Bureaucrat( std::string r_name);
+		Bureaucrat(void);
+		Bureaucrat(std::string r_name, int r_grade);
 		Bureaucrat(const Bureaucrat &obj);
 		~Bureaucrat();
+	private:
 		Bureaucrat &operator=(const Bureaucrat &obj);
+	
+	public:
+		const std::string getName();
+		int getGrade();
+		void incrementGrade();
+		void decrementGrade();
 };
 
 #endif

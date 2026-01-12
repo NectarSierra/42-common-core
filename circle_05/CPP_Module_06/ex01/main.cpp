6c642e56e3ea0b5581973a36a3556e3d51b8206a
NectarSierra <worksailleznathan@gmail.com>
2026-01-12 12:09:19 +0100

    refactor code

--- circle_05/CPP_Module_06/ex01/main.cpp
+++ circle_05/CPP_Module_06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nsaillez <nsaillez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 08:58:45 by nsaillez          #+#    #+#             */
-/*   Updated: 2026/01/12 12:01:54 by nsaillez         ###   ########.fr       */
+/*   Updated: 2026/01/12 12:07:26 by nsaillez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,14 @@ int main()
 	original.value = 123;
 
 	Data* original_ptr = &original;
-	std::cout << "original_ptr: " << original_ptr;
 	uintptr_t serialized = Serializer::serialize(original_ptr);
-	std::cout << " -> serialized: " << serialized;
 	Data* deserialized_ptr = Serializer::deserialize(serialized);
-	std::cout << " -> deserialized_ptr: " << deserialized_ptr << std::endl;
+	
+	std::cout
+	<< "original_ptr: " << original_ptr
+	<< " -> serialized: " << serialized
+	<< " -> deserialized_ptr: " << deserialized_ptr
+	<< std::endl;
 
 	if (original_ptr != deserialized_ptr)
 		std::cout << "original_ptr != deserialized_ptr" << "\n" << std::endl;

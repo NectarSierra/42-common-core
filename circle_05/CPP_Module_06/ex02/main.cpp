7247973bf27beafad65bebd79cc74bdc446091f8
NectarSierra <worksailleznathan@gmail.com>
2026-01-12 16:40:11 +0100

    change main.cpp

--- circle_05/CPP_Module_06/ex02/main.cpp
+++ circle_05/CPP_Module_06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nsaillez <nsaillez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 12:13:18 by nsaillez          #+#    #+#             */
-/*   Updated: 2026/01/12 15:15:14 by nsaillez         ###   ########.fr       */
+/*   Updated: 2026/01/12 16:35:53 by nsaillez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@
 
 int main()
 {
-	Base* test = new A();
-	Base::identify(test);
+	A a = A();
+	Base& a_ref = a;
+	Base::identify(a_ref);
 	
 	Base* generated = Base::generate();
-	Base::identify(*generated);
+	Base::identify(generated);
 	return (0);
 }

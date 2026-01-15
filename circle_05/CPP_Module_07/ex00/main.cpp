49ba2ad11c49f90a6a921ae68e25a09f53dbb6d5
Nathan Saillez <nsaillez@shi-r6-p1.s19.be>
2026-01-15 14:56:33 +0100

    finished ex00

--- circle_05/CPP_Module_07/ex00/main.cpp
+++ circle_05/CPP_Module_07/ex00/main.cpp
@@ -6,11 +6,27 @@
 /*   By: nsaillez <nsaillez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 09:39:25 by nsaillez          #+#    #+#             */
-/*   Updated: 2026/01/15 09:39:46 by nsaillez         ###   ########.fr       */
+/*   Updated: 2026/01/15 14:54:24 by nsaillez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "whatever.hpp"
+
 int main()
 {
+	int a = 2;
+	int b = 3;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return (0);
 }
--- circle_05/CPP_Module_07/ex00/whatever.hpp
+++ circle_05/CPP_Module_07/ex00/whatever.hpp
@@ -6,7 +6,24 @@
 /*   By: nsaillez <nsaillez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 09:40:13 by nsaillez          #+#    #+#             */
-/*   Updated: 2026/01/15 09:40:14 by nsaillez         ###   ########.fr       */
+/*   Updated: 2026/01/15 14:54:28 by nsaillez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+template <typename T> void swap(T& a, T& b)
+{
+	T temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T> T min(T& a, T& b)
+{
+	return ((a < b) ? a : b);
+}
+
+template <typename T> T max(T& a, T& b)
+{
+	return ((a > b) ? a : b);
+}

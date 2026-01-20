b9add8a7705370969345a365562bf42e124bc8ed
Nathan Saillez <nsaillez@shi-r6-p1.s19.be>
2026-01-20 08:44:55 +0100

    finished ex01

--- circle_05/CPP_Module_07/ex01/iter.hpp
+++ circle_05/CPP_Module_07/ex01/iter.hpp
@@ -6,7 +6,35 @@
 /*   By: nsaillez <nsaillez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 14:58:08 by nsaillez          #+#    #+#             */
-/*   Updated: 2026/01/15 14:58:09 by nsaillez         ###   ########.fr       */
+/*   Updated: 2026/01/20 08:44:37 by nsaillez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+template <typename A>
+void non_const_print(A& value)
+{
+	value += 1; 
+	std::cout << value << std::endl;
+}
+
+template <typename A>
+void const_print(const A& value)
+{
+	std::cout << value << std::endl;
+}
+
+template <typename A>
+void iter(A *adrs,const size_t len, void (*f)(A&))
+{
+	for (size_t i = 0; i < len; i++)
+		f(adrs[i]);
+}
+
+template <typename A>
+void iter(const A *adrs ,const size_t len, void (*f)(const A&))
+{
+	for (size_t i = 0; i < len; i++)
+		f(adrs[i]);
+}
--- circle_05/CPP_Module_07/ex01/main.cpp
+++ circle_05/CPP_Module_07/ex01/main.cpp
@@ -6,11 +6,30 @@
 /*   By: nsaillez <nsaillez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 14:58:11 by nsaillez          #+#    #+#             */
-/*   Updated: 2026/01/15 14:58:18 by nsaillez         ###   ########.fr       */
+/*   Updated: 2026/01/19 13:55:21 by nsaillez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+#include <iostream>
+
+void test2( const char &addrs_test)
+{
+	std::cout << addrs_test << std::endl;
+}
+
+void test( char &addrs_test)
+{
+	addrs_test += 1;
+	std::cout << addrs_test << std::endl;
+}
+
 int main()
 {
+	char i[3] = {'h', 'c', 'k'};
 	
+	::iter(i, 3, ::const_print);
+	::iter(i, 3, ::non_const_print);
+	::iter(i, 3, test);
+	::iter(i, 3, test2);
 }

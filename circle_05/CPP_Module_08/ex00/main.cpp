4c046cb61dda63e5ec9380e3f73884ffa9d38741
Nathan Saillez <nsaillez@shi-r6-p1.s19.be>
2026-02-05 14:41:20 +0100

    added try catch

--- circle_05/CPP_Module_08/ex00/main.cpp
+++ circle_05/CPP_Module_08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nsaillez <nsaillez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 09:16:23 by nsaillez          #+#    #+#             */
-/*   Updated: 2026/02/02 13:44:37 by nsaillez         ###   ########.fr       */
+/*   Updated: 2026/02/05 14:40:10 by nsaillez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,16 @@ int main(void)
 	array.insert(5);
 	array.insert(4);
 	
-	std::set<int>::iterator it = easyfind(array, 0);
-	std::cout << *it << '\n';
-	
-	it = array.find(0);
-	std::cout << *it << '\n';
-	
+	try
+	{
+		std::set<int>::iterator it = easyfind(array, 0);
+		std::cout << *it << '\n';
+		it = array.find(0);
+		std::cout << *it << '\n';
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "exception: " << e.what() << '\n';
+	}
 	return (0);
 }

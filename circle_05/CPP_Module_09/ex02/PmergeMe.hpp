9c372bebfd87ae833db7293172ce0d5301e2d44a
NectarSierra <worksailleznathan@gmail.com>
2026-05-19 12:59:56 +0200

    feat: (test) pairs

--- circle_05/CPP_Module_09/ex02/PmergeMe.hpp
+++ circle_05/CPP_Module_09/ex02/PmergeMe.hpp
@@ -6,7 +6,18 @@
 /*   By: nsaillez <nsaillez@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/10 13:20:31 by nsaillez          #+#    #+#             */
-/*   Updated: 2026/05/10 13:20:32 by nsaillez         ###   ########.fr       */
+/*   Updated: 2026/05/19 09:06:14 by nsaillez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <vector>
+
+class PmergeMe
+{
+public:
+	PmergeMe(const std::vector<int> &arr);
+	
+	PmergeMe(const PmergeMe &other);
+	~PmergeMe();
+	PmergeMe operator=(const PmergeMe &other);
+};
--- circle_05/CPP_Module_09/ex02/main.cpp
+++ circle_05/CPP_Module_09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nsaillez <nsaillez@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/09 13:19:19 by nsaillez          #+#    #+#             */
-/*   Updated: 2026/05/18 12:54:40 by nsaillez         ###   ########.fr       */
+/*   Updated: 2026/05/19 11:27:02 by nsaillez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <cstdlib>
 #include <vector>
 
-int parse(char *str)
+int strtol_checker(char *str)
 {
 	char* end;
 	long tmp;
@@ -39,35 +39,76 @@ int parse(char *str)
 	return (static_cast<int>(tmp));
 }
 
+int parser(int argc, char** argv, std::vector<int> &arr)
+{
+	for (int i = 1; i < argc; i++)
+	{
+		int tmp = strtol_checker(argv[i]);
+		if (tmp < 0)
+			return (-1);
+		for (int j = 0; static_cast<size_t>(j) < arr.size(); j++)
+		{
+			if ((i - 1) != j && tmp == arr[j])
+			{
+				std::cout << "\033[31mError: Duplicates detected\033[0m" << std::endl;
+				return (-1);
+			}
+		}
+		arr.push_back(tmp);
+	}
+	return (0);
+}
+
+struct pairs
+{
+	int largest;
+	int smallest;
+};
+
+
 int	main(int argc, char **argv)
 {
 	if (argc < 2)
 	{
-		std::cerr << "Wrong usage of program!" << std::endl;
+		std::cerr << "\033[31mError: Wrong use of program!\033[0m" << std::endl;
 		return (1);
 	}
-	std::vector<int> arr1;
-	for (int i = 1; i < argc; i++)
-	{
-		int tmp = parse(argv[i]);
-		if (tmp < 0)
-			return (1);
-		for (int j = 0; static_cast<size_t>(j) < arr1.size(); j++)
-		{
-			if ((i - 1) != j && tmp == arr1[j])
-			{
-				std::cout << "\033[31mError: Duplicates detected\033[0m" << std::endl;
-				return (1);
-			}
-			
-		}
-		arr1.push_back(tmp);
-	}
+	std::vector<int> unsorted_numbers;
+	if (parser(argc, argv, unsorted_numbers))
+		return (-1);
+	
 	std::cout << "Before:";
 	for (int i = 0; i < argc - 1; i++)
-		std::cout << " " << arr1[i];
+		std::cout << " " << unsorted_numbers[i];
 	std::cout << std::endl;
-	// parsing (int, only +, detect overflow)
+
 	
+	// pairs
+	std::vector<pairs> arr1;
+	pairs tmp;
+	for (size_t i = 0; i < unsorted_numbers.size(); i += 2)
+	{
+		if (i + 1 != unsorted_numbers.size())
+		{
+			if (unsorted_numbers[i] > unsorted_numbers[i + 1])
+			{
+				tmp.largest = unsorted_numbers[i];
+				tmp.smallest = unsorted_numbers[i + 1];
+			}
+			else
+			{
+				tmp.largest = unsorted_numbers[i + 1];
+				tmp.smallest = unsorted_numbers[i];
+			}
+		}
+		else
+		{
+			tmp.largest = -1; // since undefined -1
+			tmp.smallest = unsorted_numbers[i];
+		}
+		std::cout << "(" << tmp.largest << ", " << tmp.smallest << ")" << std::endl;
+		arr1.push_back(tmp);
+	}
+	// tris of largest
 	return (0);
 }
